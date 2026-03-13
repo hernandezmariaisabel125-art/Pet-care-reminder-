7c31308eaef01b5c935d615b427af900df848c1e
hernandezmariaisabel125-art <hernandezmariaisabel125@gmail.com>
2026-03-12 22:24:11 -0500

    se finalizó el proyecto

--- sw.js
+++ sw.js
@@ -1,46 +1,34 @@
-const CACHE_NAME = "pet care reminder v1";
 
-const URLS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/app.html",
-  "/store.html",
-  "/felinos.html",
-  "/caninos.html",
-  "/styles.css",
-  "/app.js",
-  "/manifest.json"
+
+
+ 
+  
+
+
+const CACHE_NAME = "miapp-V1!";
+// Solo mete lo básico para que no de error buscando archivos que no existen
+const ASSETS = [
+   "/",
+  "./index.html",
+  "./app.html",
+  "./store.html",
+  "./productos.html",
+  "./perros.html",
+  "./styles.css",
+  "./app.js",
 ];
 
-/* INSTALACIÓN */
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Instalando cache...");
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-/* ACTIVACIÓN */
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
-
-/* FETCH – MODO OFFLINE */
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
